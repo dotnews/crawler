0364f593bbea9b19885642b885c65cc4b040a109
Wagner Camarao <wagner@camarao.io>
2018-07-22 12:47:59 +1000

    Parse video articles

--- src/foxnews/article.js
+++ src/foxnews/article.js
@@ -6,12 +6,36 @@ const stdIn = require('fs').readFileSync(0);
   const browser = await puppeteer.launch(config);
   const page = await browser.newPage();
   const article = JSON.parse(stdIn.toString());
+  let content;
 
   await page.goto(article.href, {
     waitUntil: 'domcontentloaded',
   });
 
-  const content = await page.$eval('.main-content .article-wrap', el => {
+  await page.addScriptTag({url: 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js'});
+
+  if (isVideo(article)) {
+    try {
+      await hasVideoMeta(page);
+    } finally {
+      content = await parseVideo(page);
+    }
+  } else {
+    content = await parseText(page);
+  }
+
+  Object.assign(article, content);
+  console.log(JSON.stringify(article, 0, 2));
+  await page.close();
+  await browser.close();
+})();
+
+const isVideo = article => {
+  return article.href.includes('//video.foxnews.com/');
+};
+
+const parseText = page => {
+  return page.$eval('.main-content .article-wrap', el => {
     const title = el.querySelector('h1');
     const author = el.querySelector('.author-byline [rel=author]');
     const date = el.querySelector('.article-date time');
@@ -25,8 +49,25 @@ const stdIn = require('fs').readFileSync(0);
       body: body && body.innerHTML,
     };
   });
+};
 
-  Object.assign(article, content);
-  console.log(JSON.stringify(article, 0, 2));
-  await browser.close();
-})();
+const parseVideo = page => {
+  return page.$eval('.video-meta', el => {
+    const title = el.querySelector('h1');
+    const p = el.querySelector('p');
+    return {
+      title: title && title.innerText,
+      date: p && moment(p.innerText.trim(), 'MMM. DD, YYYY - H:mm').toJSON(),
+    };
+  });
+};
+
+const hasVideoMeta = page => {
+  return page.waitForFunction(
+    () => {
+      const p = document.querySelector('.video-meta p');
+      return p && p.innerText;
+    },
+    {timeout: 3000},
+  );
+};
